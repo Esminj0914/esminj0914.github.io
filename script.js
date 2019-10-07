79b2f469e131f7ca3ddc617b706afdc226bdf985
Esminj0914 <43858616+Esminj0914@users.noreply.github.com>
2019-10-07 09:37:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Esmin J"
+let userName = "Esminj0914";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
